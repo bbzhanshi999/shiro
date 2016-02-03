c0b7f5027ccf26481ed99982aeae9e94bd6c9ab9
bbzhanshi999 <bbzhanshi999@163.com>
2016-02-04 01:17:42 +0800

    Initial commit

---
+++ src/main/java/com.my.shiro/MyRealm1.java
@@ -0,0 +1,30 @@
+package com.my.shiro;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by Administrator on 2016/2/3 0003.
+ */
+public class MyRealm1 implements Realm {
+    public String getName() {
+        return "myrealm1";
+    }
+
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return authenticationToken instanceof UsernamePasswordToken;
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username = (String)authenticationToken.getPrincipal(); //得到用户名
+        String password = new String((char[])authenticationToken.getCredentials()); //得到密码
+        if(!"zhang".equals(username)) {
+            throw new UnknownAccountException(); //如果用户名错误
+        }
+        if(!"123".equals(password)) {
+            throw new IncorrectCredentialsException(); //如果密码错误
+        }
+//如果身份认证验证成功，返回一个 AuthenticationInfo 实现；
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
---
+++ src/main/java/com.my.shiro/Test1.java
@@ -0,0 +1,8 @@
+package com.my.shiro;
+
+/**
+ * Created by Administrator on 2016/2/3 0003.
+ */
+public class Test1 {
+
+}
